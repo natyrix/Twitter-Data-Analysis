bb522d8b69db3cd0e235ceef21cfde1cd65841ae
Natnael Melese <natrix277@gmail.com>
2022-08-10 23:33:52 +0300

    Dashboard using streamlit

--- st_dashboard/dashboard.py
+++ st_dashboard/dashboard.py
@@ -7,21 +7,22 @@ import plotly.express as px
 from add_data import db_execute_fetch
 
 st.set_page_config(page_title="Day 5", layout="wide")
-
+loaded_df = None
 def loadData():
     query = "select * from TweetInformation"
     df = db_execute_fetch(query, dbName="tweets", rdf=True)
+    loaded_df = df
     return df
 
 def selectHashTag():
-    df = loadData()
+    df = loadData() if loaded_df is None else loaded_df
     hashTags = st.multiselect("choose combaniation of hashtags", list(df['hashtags'].unique()))
     if hashTags:
         df = df[np.isin(df, hashTags).any(axis=1)]
         st.write(df)
 
 def selectLocAndAuth():
-    df = loadData()
+    df = loadData() if loaded_df is None else loaded_df
     location = st.multiselect("choose Location of tweets", list(df['place'].unique()))
     lang = st.multiselect("choose Language of tweets", list(df['language'].unique()))
 
@@ -46,7 +47,7 @@ def barChart(data, title, X, Y):
     st.altair_chart(msgChart, use_container_width=True)
 
 def wordCloud():
-    df = loadData()
+    df = loadData() if loaded_df is None else loaded_df
     cleanText = ''
     for text in df['full_text']:
         tokens = str(text).lower().split()
@@ -58,7 +59,7 @@ def wordCloud():
     st.image(wc.to_array())
 
 def stBarChart():
-    df = loadData()
+    df = loadData() if loaded_df is None else loaded_df
     dfCount = pd.DataFrame({'Tweet_count': df.groupby(['original_author'])['full_text'].count()}).reset_index()
     dfCount["original_author"] = dfCount["original_author"].astype(str)
     dfCount = dfCount.sort_values("Tweet_count", ascending=False)
@@ -67,9 +68,25 @@ def stBarChart():
     title = f"Top {num} Ranking By Number of tweets"
     barChart(dfCount.head(num), title, "original_author", "Tweet_count")
 
+def sourcePie():
+    df = loadData() if loaded_df is None else loaded_df
+    dfSourceCount = pd.DataFrame({'Tweet_count': df.groupby(['source'])['full_text'].count()}).reset_index()
+    dfSourceCount['source'] = dfSourceCount['source'].astype(str)
+    dfSourceCount = dfSourceCount.sort_values("Tweet_count", ascending=False)
+    dfSourceCount.loc[dfSourceCount['Tweet_count'] < 10, 'source'] = 'Other sources'
+    st.title("Tweet source pie chart")
+    fig = px.pie(dfSourceCount, values='Tweet_count', names='source', width=500, height=350)
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    colB1, colB2 = st.columns([2.5, 1])
+
+    with colB1:
+        st.plotly_chart(fig)
+    with colB2:
+        st.write(dfSourceCount)
 
 def langPie():
-    df = loadData()
+    df = loadData() if loaded_df is None else loaded_df
     dfLangCount = pd.DataFrame({'Tweet_count': df.groupby(['language'])['full_text'].count()}).reset_index()
     dfLangCount["language"] = dfLangCount["language"].astype(str)
     dfLangCount = dfLangCount.sort_values("Tweet_count", ascending=False)
@@ -95,3 +112,6 @@ wordCloud()
 with st.expander("Show More Graphs"):
     stBarChart()
     langPie()
+
+with st.expander("Show Other Graphs"):
+    sourcePie()
