109da95cc1c5907a59816fd004898bf97d68d4e2
Natnael Melese <natrix277@gmail.com>
2022-08-12 16:31:23 +0300

    Logger and try catch for clean_tweets_dataframe.py

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import re
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join("./scripts/")))
+from logger import logger
+
+
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -14,9 +21,13 @@ class Clean_Tweets:
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count'].index
-        df.drop(unwanted_rows, inplace=True)
-        df = df[df['polarity'] != 'polarity']
+        try:
+            unwanted_rows = df[df['retweet_count'] == 'retweet_count'].index
+            df.drop(unwanted_rows, inplace=True)
+            df = df[df['polarity'] != 'polarity']
+            logger.info("UNWANTED COLUMNS DROPPED")
+        except Exception as e:
+            logger.error(e)
 
         return df
 
@@ -24,18 +35,24 @@ class Clean_Tweets:
         """
         drop duplicate rows
         """
+        try:
 
-        df = self.df.drop_duplicates()
-
+            df = self.df.drop_duplicates()
+            logger.info("DROPPED DUPLICATES")
+        except Exception as e:
+            logger.error(e)
         return df
 
     def convert_to_datetime(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         convert column to datetime
         """
-        df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
-        df = df[df['created_at'] >= '2020-12-31']
-
+        try:
+            df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+            df = df[df['created_at'] >= '2020-12-31']
+            logger.info("DATETIME CONVERTED")
+        except Exception as e:
+            logger.error(e)
         return df
 
     def convert_to_numbers(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -43,31 +60,40 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        df['polarity'] = pd.to_numeric(df['polarity'], errors='coerce')
-        df['subjectivity'] = pd.to_numeric(df['subjectivity'], errors='coerce')
-        df['retweet_count'] = pd.to_numeric(
-            df['retweet_count'], errors='coerce')
-        df['favorite_count'] = pd.to_numeric(
-            df['favorite_count'], errors='coerce')
-        df['followers_count'] = pd.to_numeric(
-            df['followers_count'], errors='coerce')
-
+        try:
+            df['polarity'] = pd.to_numeric(df['polarity'], errors='coerce')
+            df['subjectivity'] = pd.to_numeric(df['subjectivity'], errors='coerce')
+            df['retweet_count'] = pd.to_numeric(
+                df['retweet_count'], errors='coerce')
+            df['favorite_count'] = pd.to_numeric(
+                df['favorite_count'], errors='coerce')
+            df['followers_count'] = pd.to_numeric(
+                df['followers_count'], errors='coerce')
+            logger.info("CONVERTED TO NUMBERS")
+        except Exception as e:
+            logger.error(e)
         return df
 
     def remove_non_english_tweets(self, df: pd.DataFrame) -> pd.DataFrame:
         """
         remove non english tweets from lang
         """
-
-        df = df.query("lang == 'en' ")
-
+        try:
+            df = df.query("lang == 'en' ")
+            logger.info("NON ENGLISH TWEETS REMOVED")
+        except Exception as e:
+            logger.error(e)
         return df
 
     def extract_twitter_source(self, source: str):
         """
         returnssource device from source text
         """
-        res = re.split('<|>', source)[2].strip()
+        try:
+            res = re.split('<|>', source)[2].strip()
+            logger.info("SOURCE EXTRACTED")
+        except Exception as e:
+            logger.error(e)
         return res
 
     def remove_place_characters(self, df: pd.DataFrame):
@@ -75,8 +101,12 @@ class Clean_Tweets:
         removes non-alphanumeric characters with the exception of underscore hyphen and space
         from the specified column
         """
-
-        df["place"] = df["place"].apply(
-            lambda text: re.sub("[^a-zA-Z0-9\s_-]", "", text))
+        try:
+            df["place"] = df["place"].apply(
+                lambda text: re.sub("[^a-zA-Z0-9\s_-]", "", text))
+            logger.info("NON-ALPHANUMERIC CHARACTERS REMOVED FROM PLACE")
+            
+        except Exception as e:
+            logger.error(e)
 
         return df
